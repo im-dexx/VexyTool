aa159a25182c57ca06863de0ef4c2bdf1d8bd908
dex <97322316+im-dexx@users.noreply.github.com>
2022-02-22 20:35:47 -0500

    Update vex.py

--- Vex Loader/vex.py
+++ Vex Loader/vex.py
@@ -53,12 +53,6 @@ elif sys.platform == "win32":
     osys = "win"
 
 def check4config():
-    githubvex2 = requests.get("https://raw.githubusercontent.com/im-dexx/VexyTool/main/Vex%20Loader/vex.py").text
-    if updater != githubvex2:
-        print(f"{Fore.RED}* {Fore.WHITE} Vex is outdated, updating...")
-        time.sleep(1)
-        update = open("vex.py", "w+")
-        update.write(githubvex2)
     if not path.exists("config"):
         # Reinstall Config
         error("Configuration not found, re-installing.")
